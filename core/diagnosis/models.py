352c08822a53945a2f1c298d7706a3c490edc833
Davide <luisdavcasta@gmail.com>
2025-02-25 16:23:45 -0500

    udate all

---
+++ core/diagnosis/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from core.users.models import User
+
+# Create your models here.
+class Diagnosis(models.Model):
+    diagnosis = models.TextField("diagnosis", blank=True, null=True)
+    events = models.TextField("events", blank=True, null=True)
+    water = models.CharField("water", max_length=200, blank=True, null=True)
+    feeding = models.CharField("feeding", max_length=200, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField('created_at', auto_now_add=True)
+    
+    class Meta:
+        db_table = 'Diagnosis'
+        ordering = ['id'] 
